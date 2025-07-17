81d0c63599e1edb8749faa180776c7c5806ba497
ikomaz97 <ikomaz97@gmail.com>
2025-07-17 18:59:09 +0400

    17.07.2025 + botton XXX

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -23,7 +23,7 @@ export const TodolistItem = ({title, tasks}: Props) => {
                     <li key={task.id}>
                       <input type="checkbox" checked={task.isDone} />
                       <span>{task.title}</span>
-                        <button>X</button>
+                        <button>XXX</button>
                     </li>
                 )
               })}
