24e2ed42cd5ff5c236604c0415c7780a08fc6c30
ikomaz97 <ikomaz97@gmail.com>
2025-07-17 20:04:46 +0400

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todolist-redax-toolkit/', // ⚠️ важно: имя репозитория
+  base: '/todolist-redax-toolkit/',// ⚠️ важно: имя репозитория
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Папка сборки (по умолчанию)
+  }
 })
