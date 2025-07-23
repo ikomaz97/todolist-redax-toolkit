edc99137fd389e466d12d86a5a78424d24755364
ikomaz97 <ikomaz97@gmail.com>
2025-07-23 18:22:42 +0400

    firs commit 10

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -2,12 +2,13 @@ import type {Task} from './App'
 import {Button} from './Button'
 
 type Props = {
-  title: string
-  tasks: Task[]
-    deleteTask: (taskId: number) => void
+  title: string,
+  tasks: Task[],
+  deleteTask: (taskId: number) => void,
+    onClick?: () => void
 }
 
-export const TodolistItem = ({title, tasks, deleteTask}: Props) => {
+export const TodolistItem = ({title, tasks, deleteTask, }: Props) => {
   return (
       <div>
         <h3>{title}</h3>
@@ -24,7 +25,7 @@ export const TodolistItem = ({title, tasks, deleteTask}: Props) => {
                     <li key={task.id}>
                       <input type="checkbox" checked={task.isDone} />
                       <span>{task.title}</span>
-                        <button onClick={() => deleteTask(task.id)}>x</button>
+                        <Button title={'x'} onClick={() => deleteTask(task.id)} />
                     </li>
                 )
               })}
