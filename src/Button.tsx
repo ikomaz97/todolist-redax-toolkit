c58b3f0499c8b3f225a116c5ae351d026f987c60
ikomaz97 <ikomaz97@gmail.com>
2025-07-23 18:21:43 +0400

    firs commit 9

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,10 @@
 type Props = {
   title: string
+  onClick: () => void
 }
 
 
 
-export const Button = ({ title }: Props) => {
-  return <button>{title}</button>
+export const Button = ({ title, onClick }: Props) => {
+  return <button onClick={onClick}>{title}</button>
 }
