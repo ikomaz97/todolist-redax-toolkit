e7a65cf5aada0453dff9d1f5e0ddc9d700f030c2
ikomaz97 <ikomaz97@gmail.com>
2025-07-23 13:33:12 +0400

    firs commit

---
+++ src/TodolistItem.tsx
@@ -0,0 +1,40 @@
+import type {Task} from './App'
+import {Button} from './Button'
+
+type Props = {
+  title: string
+  tasks: Task[]
+    deleteTask: (taskId: number) => void
+}
+
+export const TodolistItem = ({title, tasks}: Props) => {
+  return (
+      <div>
+        <h3>{title}</h3>
+        <div>
+          <input/>
+          <Button title={'+'} />
+        </div>
+        {tasks.length === 0 ? (
+            <p>Тасок нет</p>
+        ) : (
+            <ul>
+              {tasks.map(task => {
+                return (
+                    <li key={task.id}>
+                      <input type="checkbox" checked={task.isDone} />
+                      <span>{task.title}</span>
+                        <button onClick={() => alert(task.id)}>x</button>
+                    </li>
+                )
+              })}
+            </ul>
+        )}
+        <div>
+          <Button title={'AllLLL'} />
+          <Button title={'Active'} />
+          <Button title={'Completed'} />
+        </div>
+      </div>
+  )
+}
