404996fc9a82e9cdd0ddb30b96abed44f7873b11
ikomaz97 <ikomaz97@gmail.com>
2025-07-24 09:04:46 +0400

    first commit 20

--- src/App.tsx
+++ src/App.tsx
@@ -6,46 +6,48 @@ import {v1} from "uuid";
 export type FilterValues = 'all' | 'active' | 'completed'
 
 export type Task = {
-  id: string
-  title: string
-  isDone: boolean
+    id: string
+    title: string
+    isDone: boolean
 }
 
 export const App = () => {
-  const [filter, changeFilter] = useState<FilterValues>('all')
-  const [tasks, setTasks] = useState<Task[]>([
-    { id: v1(), title: 'HTML&CSS', isDone: true },
-    { id: v1(), title: 'JS', isDone: true },
-    { id: v1(), title: 'ReactJS', isDone: false },
-  ])
+    const [filter, changeFilter] = useState<FilterValues>('all')
+    const [tasks, setTasks] = useState<Task[]>([
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'ReactJS', isDone: false},
+    ])
 
-  const deleteTask = (taskId: string) => {
-    const filteredTasks = tasks.filter(task => {
-      return task.id !== taskId
-    })
-    setTasks(filteredTasks)
-  }
+    const deleteTask = (taskId: string) => {
+        const filteredTasks = tasks.filter(task => {
+            return task.id !== taskId
+        })
+        setTasks(filteredTasks)
+    }
 
-  const createTask = () => {
-    alert('Создание таски')
-  }
+    const createTask = (title: string) => {
+        const newTask = {id: v1(), title, isDone: false}
+        const newTasks = [newTask, ...tasks]
+        setTasks(newTasks)
+    }
 
-  let filteredTasks = tasks
-  if (filter === 'active') {
-    filteredTasks = tasks.filter(task => !task.isDone)
-  }
-  if (filter === 'completed') {
-    filteredTasks = tasks.filter(task => task.isDone)
-  }
+    let filteredTasks = tasks
+    if (filter === 'active') {
+        filteredTasks = tasks.filter(task => !task.isDone)
+    }
+    if (filter === 'completed') {
+        filteredTasks = tasks.filter(task => task.isDone)
+    }
 
-  return (
-      <div className="app">
-        <TodolistItem title="What to learn"
-                      tasks={filteredTasks}
-                      deleteTask={deleteTask}
-                      changeFilter={changeFilter}
-                      createTask={createTask}
-        />
-      </div>
-  )
+    return (
+        <div className="app">
+            <TodolistItem title="What to learn"
+                          tasks={filteredTasks}
+                          deleteTask={deleteTask}
+                          changeFilter={changeFilter}
+                          createTask={createTask}
+            />
+        </div>
+    )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,43 +1,50 @@
 import type {FilterValues, Task} from './App'
 import {Button} from './Button'
+import {useRef} from 'react'
 
 type Props = {
-  title: string,
-  tasks: Task[],
-  deleteTask: (taskId: string) => void,
+    title: string,
+    tasks: Task[],
+    deleteTask: (taskId: string) => void,
     onClick?: () => void
     changeFilter: (filter: FilterValues) => void,
-    createTask: () => void
+    createTask: (title: string) => void
 }
 
-export const TodolistItem = ({title, tasks, deleteTask, changeFilter, createTask }: Props) => {
-  return (
-      <div>
-        <h3>{title}</h3>
+export const TodolistItem = ({title, tasks, deleteTask, changeFilter, createTask}: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null)
+    return (
         <div>
-          <input/>
-            <Button title={'+'} onClick={createTask} />
+            <h3>{title}</h3>
+            <div>
+                <input ref={inputRef}/>
+                <Button title={'+'} onClick={() => {
+                    if (inputRef.current) {
+                        createTask(inputRef.current.value)
+                        inputRef.current.value = ''
+                    }
+                }}/>
+            </div>
+            {tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone}/>
+                                <span>{task.title}</span>
+                                <Button title={'x'} onClick={() => deleteTask(task.id)}/>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+            <div>
+                <Button title={'All'} onClick={() => changeFilter('all')}/>
+                <Button title={'Active'} onClick={() => changeFilter('active')}/>
+                <Button title={'Completed'} onClick={() => changeFilter('completed')}/>
+            </div>
         </div>
-        {tasks.length === 0 ? (
-            <p>Тасок нет</p>
-        ) : (
-            <ul>
-              {tasks.map(task => {
-                return (
-                    <li key={task.id}>
-                      <input type="checkbox" checked={task.isDone} />
-                      <span>{task.title}</span>
-                        <Button title={'x'} onClick={() => deleteTask(task.id)} />
-                    </li>
-                )
-              })}
-            </ul>
-        )}
-        <div>
-          <Button title={'All'} onClick={()=> changeFilter('all')} />
-          <Button title={'Active'} onClick={()=> changeFilter('active')} />
-          <Button title={'Completed'} onClick={()=> changeFilter('completed')} />
-        </div>
-      </div>
-  )
+    )
 }
