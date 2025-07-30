d7eb2615182fef1960510a933766513e5855142d
ikomaz97 <ikomaz97@gmail.com>
2025-07-30 13:29:49 +0400

    sprint 02 02 14

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,7 +1,23 @@
+import {useState} from "react";
+
 type Props = {
     value: string
 }
 
 export const EditableSpan = ({ value }: Props) => {
-    return <span>{value}</span>
+    const [isEditMode, setIsEditMode] = useState(false)
+
+    const turnOnEditMode = () => {
+        setIsEditMode(true)
+    }
+
+    return (
+        <>
+            {isEditMode ? (
+                <input value={value} autoFocus />
+            ) : (
+                <span onDoubleClick={turnOnEditMode}>{value}</span>
+            )}
+        </>
+    )
 }
