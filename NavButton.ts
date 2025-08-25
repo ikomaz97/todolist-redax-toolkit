858e7940a9e5792fada2214218ac2da51bc47791
ikomaz97 <ikomaz97@gmail.com>
2025-08-25 11:49:19 +0400

    sprint 02 4 2

--- NavButton.ts
+++ NavButton.ts
@@ -1,11 +1,11 @@
-import { styled } from '@mui/material/styles'
+import {styled} from '@mui/material/styles'
 import Button from '@mui/material/Button'
 
 type Props = {
     background?: string
 }
 
-export const NavButton = styled(Button)<Props>(({ background, theme }) => ({
+export const NavButton = styled(Button)<Props>(({background, theme}) => ({
     minWidth: '110px',
     fontWeight: 'bold',
     boxShadow: `0 0 0 2px ${theme.palette.primary.dark}, 4px 4px 0 0 ${theme.palette.primary.dark}`,
