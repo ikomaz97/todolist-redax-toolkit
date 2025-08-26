01262b742ba3443edb957a32cb8aef3fcf859896
ikomaz97 <ikomaz97@gmail.com>
2025-08-26 18:36:40 +0400

    sprint 02 4 18

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -23,3 +23,20 @@ test('correct todolist should be deleted', () => {
     // удалится нужный тудулист, не любой
     expect(endState[0].id).toBe(todolistId2)
 })
+
+
+test('correct todolist should be created', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    const startState: Todolist[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+
+    const title = 'New todolist'
+    const endState = todolistsReducer(startState, createTodolistAC(title))
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(title)
+})
