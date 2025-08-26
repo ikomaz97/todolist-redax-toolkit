85a2fd770deb1a90c59063a86b4688c13a4c3674
ikomaz97 <ikomaz97@gmail.com>
2025-08-26 18:49:12 +0400

    sprint 02 4 20

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -1,7 +1,7 @@
 import {v1} from 'uuid'
 import { expect, test } from 'vitest'
 import type {Todolist} from '../App'
-import {deleteTodolistAC, todolistsReducer} from './todolists-reducer'
+import {createTodolistAC, deleteTodolistAC, todolistsReducer} from './todolists-reducer'
 
 test('correct todolist should be deleted', () => {
     const todolistId1 = v1()
