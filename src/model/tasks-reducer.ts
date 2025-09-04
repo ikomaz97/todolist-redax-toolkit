79cce34b30b6df5703971ee1147651e1580f1f9e
ikomaz97 <ikomaz97@gmail.com>
2025-09-04 14:36:07 +0400

    sprint 02 4 44

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,17 +1,32 @@
 import type {TasksState} from '../App'
 
+
 const initialState: TasksState = {}
 
+export const deleteTaskAC = (payload: { todolistId: string; taskId: string }) => {
+    return  {type: 'delete_task', payload} as const}
 
 
-export const tasksReducer = (state: TasksState = initialState, action: Actions): TasksState => {
+
+
+
+export type DeleteTaskAction = ReturnType<typeof deleteTaskAC>
+
+type Actions = DeleteTaskAction
+
+
+export const tasksReducer = (task: TasksState = initialState, action: Actions): TasksState => {
     switch (action.type) {
-        case '': {
-            return state
+        case 'delete_task': {
+            const { todolistId, taskId } = action.payload
+            return {
+                ...task,
+                [todolistId]: task[todolistId].filter(t => t.id !== taskId)
+            }
         }
+
+
         default:
-            return state
+            return task
     }
 }
-
-type Actions = any
