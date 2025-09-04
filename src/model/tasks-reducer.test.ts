4c00978ca8f493c71ec5c82bc66206cd66db2d20
ikomaz97 <ikomaz97@gmail.com>
2025-09-04 14:36:37 +0400

    sprint 02 4 45

--- src/model/tasks-reducer.test.ts
+++ src/model/tasks-reducer.test.ts
@@ -1,7 +1,7 @@
 import { beforeEach, expect, test } from 'vitest'
 import type {TasksState} from '../App'
 import {createTodolistAC} from "./todolists-reducer.ts";
-import {tasksReducer} from "./tasks-reducer.ts";
+import {deleteTaskAC, tasksReducer} from "./tasks-reducer.ts";
 
 let startState: TasksState = {}
 
