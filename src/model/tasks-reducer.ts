d6b3f7403d53a99fac42828ab20c10d3c97ba05b
ikomaz97 <ikomaz97@gmail.com>
2025-09-04 19:44:59 +0400

    sprint 02 4 50

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,40 +1,42 @@
-import type {TasksState} from '../App'
-
+import type { TasksState } from '../App'
+import { v1 } from "uuid";
 
 const initialState: TasksState = {}
 
 export const deleteTaskAC = (payload: { todolistId: string; taskId: string }) => {
-    return  {type: 'delete_task', payload} as const}
+    return { type: 'delete_task', payload } as const
+}
 
-export const createTaskAC =()
-
-
+export const createTaskAC = (payload: { todolistId: string; title: string }) => {
+    return { type: 'create_task', payload } as const
+}
 
 export type DeleteTaskAction = ReturnType<typeof deleteTaskAC>
 export type CreateTaskAction = ReturnType<typeof createTaskAC>
 
-type Actions =  | DeleteTaskAction
-                | CreateTaskAction
+type Actions = DeleteTaskAction | CreateTaskAction
 
-
-export const tasksReducer = (task: TasksState = initialState, action: Actions): TasksState => {
+export const tasksReducer = (
+    state: TasksState = initialState,
+    action: Actions
+): TasksState => {
     switch (action.type) {
         case 'delete_task': {
             const { todolistId, taskId } = action.payload
             return {
-                ...task,
-                [todolistId]: task[todolistId].filter(t => t.id !== taskId)
+                ...state,
+                [todolistId]: state[todolistId].filter(t => t.id !== taskId)
             }
         }
-        case 'delete_task': {
-            const { todolistId, taskId } = action.payload
+        case 'create_task': {
+            const { todolistId, title } = action.payload
+            const newTask = { id: v1(), title, isDone: false }
             return {
-                ...task,
-                [todolistId]: task[todolistId].filter(t => t.id !== taskId)
+                ...state,
+                [todolistId]: [newTask, ...state[todolistId]]
             }
         }
-
         default:
-            return task
+            return state
     }
 }
