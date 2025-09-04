f6c8f688e388372bae6a5375be8a323f7698d53c
ikomaz97 <ikomaz97@gmail.com>
2025-09-04 12:50:09 +0400

    sprint 02 4 40

---
+++ src/model/tasks-reducer.ts
@@ -0,0 +1,19 @@
+import type {TasksState} from '../App'
+
+const initialState: TasksState = {}
+
+
+
+
+
+export const tasksReducer = (state: TasksState = initialState, action: Actions): TasksState => {
+    switch (action.type) {
+        case '': {
+            return state
+        }
+        default:
+            return state
+    }
+}
+
+type Actions = any
