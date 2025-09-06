943df0674389c37a1a4d99ff5c37a032c6781c3c
ikomaz97 <ikomaz97@gmail.com>
2025-09-06 15:24:55 +0400

    sprint 02 4 53

--- src/model/tasks-reducer.test.ts
+++ src/model/tasks-reducer.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, expect, test } from 'vitest'
 import type {TasksState} from '../App'
-import {createTaskAC, deleteTaskAC, tasksReducer} from "./tasks-reducer.ts";
+import {changeTaskStatusAC, createTaskAC, deleteTaskAC, tasksReducer} from "./tasks-reducer.ts";
 
 let startState: TasksState = {}
 
@@ -55,4 +55,19 @@ test('correct task should be created at correct array', () => {
     expect(endState.todolistId2[0].id).toBeDefined()    // id сгенерирован
     expect(endState.todolistId2[0].title).toBe('juice') // название совпадает
     expect(endState.todolistId2[0].isDone).toBe(false)  // новая задача не выполнена
+})
+
+test('correct task should change its status', () => {
+    const endState = tasksReducer(
+        startState,
+        changeTaskStatusAC({ todolistId: 'todolistId2', taskId: '2', isDone: false })
+    )
+
+    // Проверяем, что статус изменился
+    expect(endState.todolistId2[1].isDone).toBe(false)
+
+    // Проверяем, что другие задачи остались неизменными
+    expect(endState.todolistId2[0].isDone).toBe(false)
+    expect(endState.todolistId2[2].isDone).toBe(false)
+    expect(endState.todolistId1[1].isDone).toBe(true)
 })
