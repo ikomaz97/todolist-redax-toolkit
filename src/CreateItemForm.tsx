711e7ef163bcc63407ae11d9579be0b2f88d7a12
ikomaz97 <ikomaz97@gmail.com>
2025-09-10 13:10:44 +0400

    sprint 03 1 3

--- src/CreateItemForm.tsx
+++ src/CreateItemForm.tsx
@@ -3,52 +3,48 @@ import TextField from '@mui/material/TextField'
 import AddBoxIcon from '@mui/icons-material/AddBox'
 import IconButton from '@mui/material/IconButton'
 
-
 type Props = {
-    onCreateItem: (title: string) => void
+  onCreateItem: (title: string) => void
 }
 
-export const CreateItemForm = ({onCreateItem}: Props) => {
-    const [title, setTitle] = useState('')
-    const [error, setError] = useState<string | null>(null)
+export const CreateItemForm = ({ onCreateItem }: Props) => {
+  const [title, setTitle] = useState('')
+  const [error, setError] = useState<string | null>(null)
 
-    const createItemHandler = () => {
-        const trimmedTitle = title.trim()
-        if (trimmedTitle !== '') {
-            onCreateItem(trimmedTitle)
-            setTitle('')
-        } else {
-            setError('Title is required')
-        }
+  const createItemHandler = () => {
+    const trimmedTitle = title.trim()
+    if (trimmedTitle !== '') {
+      onCreateItem(trimmedTitle)
+      setTitle('')
+    } else {
+      setError('Title is required')
     }
+  }
 
-    const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value)
-        setError(null)
+  const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value)
+    setError(null)
+  }
+
+  const createItemOnEnterHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      createItemHandler()
     }
+  }
 
-    const createItemOnEnterHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            createItemHandler()
-        }
-    }
-
-    return (
-        <div>
-            <TextField label={'Enter a title'}
-                       variant={'outlined'}
-                       className={error ? 'error' : ''}
-                       value={title}
-                       size={'small'}
-                       error={!!error}
-                       helperText={error}
-                       onChange={changeTitleHandler}
-                       onKeyDown={createItemOnEnterHandler}/>
-            <IconButton onClick={createItemHandler} color={'primary'}>
-                <AddBoxIcon/>
-            </IconButton>
-
-
-        </div>
-    )
+  return (
+      <div>
+        <TextField label={'Enter a title'}
+                   variant={'outlined'}
+                   value={title}
+                   size={'small'}
+                   error={!!error}
+                   helperText={error}
+                   onChange={changeTitleHandler}
+                   onKeyDown={createItemOnEnterHandler}/>
+        <IconButton onClick={createItemHandler} color={'primary'}>
+          <AddBoxIcon />
+        </IconButton>
+      </div>
+  )
 }
