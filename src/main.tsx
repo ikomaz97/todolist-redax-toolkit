b9f9708a0d1b866731aa67f122bc298fc4c5c6f4
ikomaz97 <ikomaz97@gmail.com>
2025-09-10 14:11:30 +0400

    sprint 03 1 14

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import { App } from './app/App.tsx'
+import {Provider} from 'react-redux'
+import {App} from './app/App'
+import {store} from './app/store'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
