0baa5cae78094a4be84374bbb8e3e1590e84c396
ikomaz97 <ikomaz97@gmail.com>
2025-09-14 16:12:30 +0400

    sprint 03 1 33

--- src/model/tasks-selectors.ts
+++ src/model/tasks-selectors.ts
@@ -1,4 +1,4 @@
-import type { TasksState } from '../app/App'
-import type { RootState } from '../app/store'
+import type {TasksState} from '../app/App'
+import type {RootState} from '../app/store'
 
 export const selectTasks = (state: RootState): TasksState => state.tasks
