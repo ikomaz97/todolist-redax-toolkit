716456c459c9a337270117cd211f151a3cb81117
ikomaz97 <ikomaz97@gmail.com>
2025-09-17 14:25:02 +0400

    sprint 03 1 55

---
+++ src/common/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
+import {selectThemeMode} from "@/app/app-selectors.ts";
+import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
+import {getTheme} from "@/common/theme/theme.ts";
+import {changeThemeModeAC} from "@/app/app-reducer.ts";
+import {NavButton} from "@/NavButton.ts";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import {containerSx} from "@/TodolistItem.styles.ts";
+import IconButton from "@mui/material/IconButton";
+import Switch from "@mui/material/Switch";
+
+
+export const Header = () => {
+    const themeMode = useAppSelector(selectThemeMode)
+
+    const dispatch = useAppDispatch()
+
+    const theme = getTheme(themeMode)
+
+    const changeMode = () => {
+        dispatch(changeThemeModeAC({themeMode: themeMode === 'light' ? 'dark' : 'light'}))
+    }
+
+    return (
+        <AppBar position="static" sx={{mb: '30px'}}>
+            <Toolbar>
+                <Container maxWidth={'lg'} sx={containerSx}>
+                    <IconButton color="inherit">
+                        <MenuIcon/>
+                    </IconButton>
+                    <div>
+                        <NavButton>Sign in</NavButton>
+                        <NavButton>Sign up</NavButton>
+                        <NavButton background={theme.palette.primary.dark}>Faq</NavButton>
+                        <Switch color={'default'} onChange={changeMode} />
+                    </div>
+                </Container>
+            </Toolbar>
+        </AppBar>
+)
+}
