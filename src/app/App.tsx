aacfb21c5f4be271872abfce51cdc7dac0f2b7ee
ikomaz97 <ikomaz97@gmail.com>
2025-09-18 13:51:08 +0400

    sprint 03 1 69

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,11 @@
 import './App.css'
-import {ThemeProvider} from '@mui/material/styles'
+import {Main} from '@/app/Main'
+import {Header} from '@/common/components/Header/Header'
+import {useAppSelector} from '@/common/hooks/useAppSelector'
+import {getTheme} from '@/common/theme/theme'
 import CssBaseline from '@mui/material/CssBaseline'
-import {Header} from "@/common/components/Header/Header.tsx";
-import {Main} from "@/app/Main.tsx";
-import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
-import {selectThemeMode} from "@/app/app-selectors.ts";
-import {getTheme} from "@/common/theme/theme.ts";
-
-// Добавьте определение типа FilterValues
-export type FilterValues = 'all' | 'active' | 'completed'
-
-export type Todolist = {
-  id: string
-  title: string
-  filter: FilterValues
-}
-
-export type Task = {
-  id: string
-  title: string
-  isDone: boolean
-}
-
-export type TasksState = Record<string, Task[]>
+import {ThemeProvider} from '@mui/material/styles'
+import {selectThemeMode} from './app-selectors'
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
