37d839db7a8d7ecb1f40b74ad90c8eca3308f955
ikomaz97 <ikomaz97@gmail.com>
2025-09-18 13:34:53 +0400

    sprint 03 1 64

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,66 +1,18 @@
 import Grid from "@mui/material/Grid2";
 import {CreateItemForm} from "@/CreateItemForm.tsx";
-import Paper from "@mui/material/Paper";
-import {TodolistItem} from "@/TodolistItem.tsx";
 import Container from "@mui/material/Container";
 import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
-import {
-    changeTodolistFilterAC,
-    changeTodolistTitleAC,
-    createTodolistAC,
-    deleteTodolistAC
-} from "@/model/todolists-reducer";
-import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
-import {selectTodolists} from "@/model/todolists-selectors.ts";
-import {selectTasks} from "@/model/tasks-selectors.ts";
-import {changeTaskStatusAC, changeTaskTitleAC, createTaskAC, deleteTaskAC} from "@/model/tasks-reducer.ts";
-import {FilterValues} from "@/app/App.tsx";
+import {createTodolistAC} from "@/model/todolists-reducer";
+import {Todolists} from "@/Todolists.tsx";
 
 
 export const Main = () => {
     const dispatch = useAppDispatch()
 
 
-
-
-    const todolists = useAppSelector(selectTodolists)
-    const tasks = useAppSelector(selectTasks)
-
-
-
-
-    const changeFilter = (todolistId: string, filter: FilterValues) => {
-        dispatch(changeTodolistFilterAC({id: todolistId, filter}))
-    }
-
     const createTodolist = (title: string) => {
         dispatch(createTodolistAC(title))
     }
-
-    const deleteTodolist = (todolistId: string) => {
-        dispatch(deleteTodolistAC({id: todolistId}))
-    }
-
-    const changeTodolistTitle = (todolistId: string, title: string) => {
-        dispatch(changeTodolistTitleAC({id: todolistId, title}))
-    }
-
-    const deleteTask = (todolistId: string, taskId: string) => {
-        dispatch(deleteTaskAC({todolistId, taskId}))
-    }
-
-    const createTask = (todolistId: string, title: string) => {
-        dispatch(createTaskAC({todolistId, title}))
-    }
-
-    const changeTaskStatus = (todolistId: string, taskId: string, isDone: boolean) => {
-        dispatch(changeTaskStatusAC({todolistId, taskId, isDone}))
-    }
-
-    const changeTaskTitle = (todolistId: string, taskId: string, title: string) => {
-        dispatch(changeTaskTitleAC({todolistId, taskId, title}))
-    }
-
 
 
 
@@ -70,32 +22,7 @@ export const Main = () => {
                 <CreateItemForm onCreateItem={createTodolist}/>
             </Grid>
             <Grid container spacing={4}>
-                {todolists.map(todolist => {
-                    const todolistTasks = tasks[todolist.id]
-                    let filteredTasks = todolistTasks
-                    if (todolist.filter === 'active') {
-                        filteredTasks = todolistTasks.filter(task => !task.isDone)
-                    }
-                    if (todolist.filter === 'completed') {
-                        filteredTasks = todolistTasks.filter(task => task.isDone)
-                    }
-
-                    return (
-                        <Grid key={todolist.id}>
-                            <Paper sx={{p: '0 20px 20px 20px'}}>
-                                <TodolistItem todolist={todolist}
-                                              tasks={filteredTasks}
-                                              deleteTask={deleteTask}
-                                              changeFilter={changeFilter}
-                                              createTask={createTask}
-                                              changeTaskStatus={changeTaskStatus}
-                                              deleteTodolist={deleteTodolist}
-                                              changeTaskTitle={changeTaskTitle}
-                                              changeTodolistTitle={changeTodolistTitle}/>
-                            </Paper>
-                        </Grid>
-                    )
-                })}
+             <Todolists/>
             </Grid>
         </Container>
     )
