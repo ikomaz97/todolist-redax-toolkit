4ec1ce0837241b687672fba5c011d8e719baf42e
Chernov-V <ikomaz97@gmail.com>
2025-10-09 12:39:50 +0400

    sprint 03 63

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,9 @@
 import axios from "axios"
 
-const token = "cbb13dd0-42a7-409e-b222-cec62547608b"
-const apiKey = "8f8b8a12-f302-419e-8f3b-a77e020bdfc5"
-
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
-    "API-KEY": apiKey,
+    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    "API-KEY": import.meta.env.VITE_API_KEY,
   },
 })
