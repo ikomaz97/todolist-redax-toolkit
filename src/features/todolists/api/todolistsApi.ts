db75185cfb4eba121b187a40b2820ed54dc97ec4
Chernov-V <ikomaz97@gmail.com>
2025-10-15 18:22:33 +0400

    sprint 04 8

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -11,9 +11,7 @@ export const todolistsApi = {
     return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
   },
   createTodolist(title: string) {
-    return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", {
-      title,
-    })
+    return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
   },
   deleteTodolist(id: string) {
     return instance.delete<BaseResponse>(`/todo-lists/${id}`)
