80ac87621356c7d710e28cd56df53e3daa5f316b
Chernov-V <ikomaz97@gmail.com>
2025-10-17 12:43:55 +0400

    sprint 04 01 22

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,20 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+export type ThemeMode = "light" | "dark"
+
+export type AppState = {
+  themeMode: ThemeMode
+}
+
 export const appSlice = createSlice({
   name: "app",
   initialState: {
     themeMode: "light" as ThemeMode,
   },
-  // reducers состоит из подредьюсеров, эквивалентных одному оператору case в switch
+  selectors: {
+    selectThemeMode: (state) => state.themeMode,
+  },
   reducers: (create) => ({
+    // 🌗 Переключение темы (light/dark)
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
-      // логика в подредьюсерах мутабельная, а иммутабельность достигается благодаря immer.js
       state.themeMode = action.payload.themeMode
     }),
   }),
 })
 
-// action creator достается из appSlice.actions
+// Экспорт action creator
 export const { changeThemeModeAC } = appSlice.actions
-// reducer достается из appSlice.reducer
+
+// Экспорт reducer
 export const appReducer = appSlice.reducer
