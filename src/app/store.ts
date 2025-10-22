d6a361bad59af4f6af909d815e94890d1f68a5bb
Chernov-V <ikomaz97@gmail.com>
2025-10-22 09:29:22 +0400

    sprint 04 01 40

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { appReducer, appSlice } from "./app-slice.ts"
-import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice.ts"
-import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice.ts"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { appReducer } from "./app-reducer"
+import { tasksReducer } from "@/features/todolists/model/tasks-reducer"
+import { todolistsReducer } from "@/features/todolists/model/todolists-reducer"
+
+// объединение reducer'ов с помощью combineReducers
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+})
 
 // создание store
 export const store = configureStore({
-  reducer: {
-    [tasksSlice.name]: tasksReducer,
-    [todolistsSlice.name]: todolistsReducer,
-    [appSlice.name]: appReducer,
-  },
+  reducer: rootReducer,
 })
 
 // автоматическое определение типа всего объекта состояния
