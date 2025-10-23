3d58a576ba0114f3f0de7320bd91278459e8649b
Chernov-V <ikomaz97@gmail.com>
2025-10-23 18:44:36 +0400

    sprint 04 01 46

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,17 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit"
-import { appReducer } from "./app-reducer"
-import { tasksReducer } from "@/features/todolists/model/tasks-reducer"
-import { todolistsReducer } from "@/features/todolists/model/todolists-reducer"
+import { configureStore } from "@reduxjs/toolkit"
+import { appReducer, appSlice } from "./app-slice"
+import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
+import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice.ts"
 
-// объединение reducer'ов с помощью combineReducers
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-})
-
-// создание store
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    [tasksSlice.name]: tasksReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [appSlice.name]: appReducer,
+  },
 })
 
-// автоматическое определение типа всего объекта состояния
 export type RootState = ReturnType<typeof store.getState>
-// автоматическое определение типа метода dispatch
 export type AppDispatch = typeof store.dispatch
 
 // для возможности обращения к store в консоли браузера
