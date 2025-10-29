39c0bf353ba9b8df86d07c974c6473dcb36d30fd
Chernov-V <ikomaz97@gmail.com>
2025-10-29 19:32:57 +0400

    sprint 04 02 02

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -8,20 +8,15 @@ export const appSlice = createSlice({
   selectors: {
     selectThemeMode: (state) => state.themeMode,
   },
-  // reducers состоит из подредьюсеров, эквивалентных одному оператору case в switch
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
-      // логика в подредьюсерах мутабельная, а иммутабельность достигается благодаря immer.js
       state.themeMode = action.payload.themeMode
     }),
   }),
 })
 
-// action creator достается из appSlice.actions
+export const { selectThemeMode } = appSlice.selectors
 export const { changeThemeModeAC } = appSlice.actions
-// reducer достается из appSlice.reducer
 export const appReducer = appSlice.reducer
 
 export type ThemeMode = "dark" | "light"
-
-export const { selectThemeMode } = appSlice.selectors
