855ad050608a5669ea7d9245eec929d1a669588a
Chernov-V <ikomaz97@gmail.com>
2025-10-29 19:33:28 +0400

    sprint 04 02 06

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import { App } from "@/app/App"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import { App } from "@/app/App.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
