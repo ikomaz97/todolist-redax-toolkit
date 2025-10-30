63d2e3e6f0451168c876abb724bebd0d2d13290f
Chernov-V <ikomaz97@gmail.com>
2025-10-30 18:38:50 +0400

    sprint 04 02 14

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,13 +1,13 @@
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { selectTodolists } from "@/features/todolists/model/todolists-selectors"
-import { fetchTodolistsTC } from "@/features/todolists/model/todolists-slice"
-import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
 import Grid from "@mui/material/Grid2"
 import Paper from "@mui/material/Paper"
 import { useEffect } from "react"
+import { TodolistItem } from "./TodolistItem/TodolistItem"
 
 export const Todolists = () => {
   const todolists = useAppSelector(selectTodolists)
+
   const dispatch = useAppDispatch()
 
   useEffect(() => {
