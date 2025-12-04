e17b249813cb49283c01f31c6134813f297c6e77
Chernov-V <ikomaz97@gmail.com>
2025-12-04 18:34:28 +0400

    sprint 04 02 67

--- src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,22 +1,31 @@
-import { SyntheticEvent, useState } from "react"
-import Alert from "@mui/material/Alert"
+import React, { useCallback } from "react"
 import Snackbar from "@mui/material/Snackbar"
+import MuiAlert, { AlertProps } from "@mui/material/Alert"
+
+import { useAppSelector, useAppDispatch } from "@/common/hooks"
+import { selectAppError, setAppErrorAC } from "@/app/app-slice"
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
 
 export const ErrorSnackbar = () => {
-  const [open, setOpen] = useState(true)
+  const dispatch = useAppDispatch()
+  const error = useAppSelector(selectAppError)
 
-  const handleClose = (_: SyntheticEvent | Event, reason?: string) => {
-    if (reason === "clickaway") {
-      return
-    }
-
-    setOpen(false)
-  }
+  const handleClose = useCallback(() => {
+    dispatch(setAppErrorAC({ error: null }))
+  }, [dispatch])
 
   return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-      <Alert onClose={handleClose} severity="error" variant="filled" sx={{ width: "100%" }}>
-        Error message
+    <Snackbar
+      open={error !== null}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+    >
+      <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+        {error}
       </Alert>
     </Snackbar>
   )
