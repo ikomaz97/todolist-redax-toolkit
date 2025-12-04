080d55f042a3459b4b4c939c56f1f3b55e65fd01
Chernov-V <ikomaz97@gmail.com>
2025-12-04 18:35:14 +0400

    sprint 04 02 72

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,1 +1,9 @@
 export { createAppSlice } from "./createAppSlice"
+export { handleServerAppError } from "./handleServerAppError"
+export { handleServerNetworkError } from "./handleServerNetworkError"
+
+// Compare this snippet from src/common/utils/validateEmail.ts:
+// export const validateEmail = (email: string): boolean => {
+//   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+//   return re.test(String(email).toLowerCase())
+// }
