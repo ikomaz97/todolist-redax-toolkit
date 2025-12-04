cdd97e56b99f85ee54a31a65286246e1d7f1ce7e
Chernov-V <ikomaz97@gmail.com>
2025-12-04 18:32:45 +0400

    sprint 04 02 65

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,24 @@
-import "./App.css"
-import { selectThemeMode } from "@/app/app-slice"
-import { Main } from "@/app/Main"
-import { Header } from "@/common/components/Header/Header"
-import { useAppSelector } from "@/common/hooks"
-import { getTheme } from "@/common/theme"
-import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+
+import { useAppSelector } from "@/common/hooks"
+import { selectThemeMode } from "@/app/app-slice"
+import { getTheme } from "@/common/theme"
+
+import { Header } from "@/common/components/Header/Header"
+import { Main } from "@/app/Main"
 import { ErrorSnackbar } from "@/common/components"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
-
   const theme = getTheme(themeMode)
 
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
-        <CssBaseline />
-        <Header />
-        <Main />
-        <ErrorSnackbar />
-      </div>
+      <CssBaseline />
+      <Header />
+      <Main />
+      <ErrorSnackbar />
     </ThemeProvider>
   )
 }
