ecfc94c0556112f95b4d0fcc0be0683953d57963
Chernov-V <ikomaz97@gmail.com>
2025-12-17 18:13:29 +0400

    sprint 04 04 09

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,5 +1,2 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
-import type { RootState, AppDispatch } from "@/app/store"
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export { useAppDispatch } from "./useAppDispatch"
+export { useAppSelector } from "./useAppSelector"
