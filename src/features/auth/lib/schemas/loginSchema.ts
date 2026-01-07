645c755c578004750b2f466a93e38860a79dc359
Chernov-V <ikomaz97@gmail.com>
2026-01-07 13:25:50 +0400

    sprint 04 04 33

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -1,7 +1,12 @@
-import { z } from "zod/v4"
+import { z } from "zod"
 
 export const loginSchema = z.object({
-  email: z.email(),
-  password: z.string(),
+  email: z
+    .string()
+    .min(1, "Email обязателен") // сообщение для пустого поля
+    .email("Неверный формат email"), // сообщение для невалидного email
+  password: z.string().min(1, "Пароль обязателен"),
   rememberMe: z.boolean(),
 })
+
+export type LoginInputs = z.infer<typeof loginSchema>
