22770aa52066b7a886a284c3be8db824b0d9cc35
Chernov-V <ikomaz97@gmail.com>
2026-01-09 19:23:47 +0400

    sprint 05 01 09

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { App } from "@/app/App"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
 import { BrowserRouter } from "react-router"
+import { store } from "./app/store"
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
