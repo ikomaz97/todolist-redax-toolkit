30c8fc57cc572351650100619d5a26adf72e65ef
Chernov-V <ikomaz97@gmail.com>
2026-01-09 19:24:05 +0400

    sprint 05 01 12

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,11 +1,11 @@
 import { Main } from "@/app/Main"
-import { Route, Routes } from "react-router-dom"
-import { Login } from "@/features/auth/ui/Login/Login.tsx"
 import { PageNotFound } from "@/common/components"
+import { Login } from "@/features/auth/ui/Login/Login"
+import { Route, Routes } from "react-router"
 
 export const Path = {
   Main: "/",
-  Login: "/login",
+  Login: "login",
   NotFound: "*",
 } as const
 
