5ba25605e6bd553aa248e365e7770652f5760e94
Chernov-V <ikomaz97@gmail.com>
2026-01-09 19:23:29 +0400

    sprint 05 01 06

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,50 +1,88 @@
-import { useForm } from "react-hook-form"
+import { selectThemeMode } from "@/app/app-slice"
+import { useAppSelector } from "@/common/hooks"
+import { getTheme } from "@/common/theme"
+import { type LoginInputs, loginSchema } from "@/features/auth/lib/schemas"
 import { zodResolver } from "@hookform/resolvers/zod"
-
-import { TextField, Grid, FormControl, FormGroup, Button } from "@mui/material"
-import { LoginInputs, loginSchema } from "@/features/auth/lib/schemas"
+import Button from "@mui/material/Button"
+import Checkbox from "@mui/material/Checkbox"
+import FormControl from "@mui/material/FormControl"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import FormGroup from "@mui/material/FormGroup"
+import FormLabel from "@mui/material/FormLabel"
+import Grid from "@mui/material/Grid"
+import TextField from "@mui/material/TextField"
+import { Controller, type SubmitHandler, useForm } from "react-hook-form"
+import styles from "./Login.module.css"
 
 export const Login = () => {
+  const themeMode = useAppSelector(selectThemeMode)
+
+  const theme = getTheme(themeMode)
+
   const {
     register,
     handleSubmit,
+    reset,
+    control,
     formState: { errors },
   } = useForm<LoginInputs>({
     resolver: zodResolver(loginSchema),
     defaultValues: { email: "", password: "", rememberMe: false },
-    // при желании можно включить триггеры: mode: 'onBlur' | 'onChange'
   })
 
-  const onSubmit = (data: LoginInputs) => {
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
     console.log(data)
+    reset()
   }
 
   return (
     <Grid container justifyContent={"center"}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+                href="https://social-network.samuraijs.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
           <FormGroup>
+            <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
             <TextField
-              label="Email"
+              type="password"
+              label="Password"
               margin="normal"
               error={!!errors.email}
-              helperText={errors.email ? String(errors.email.message) : ""}
-              {...register("email")}
-            />
-
-            <TextField
-              label="Пароль"
-              type="password"
-              margin="normal"
-              error={!!errors.password}
-              helperText={errors.password ? String(errors.password.message) : ""}
               {...register("password")}
             />
-
-            {/* пример для чекбокса: */}
-            {/* <Controller ... /> или register('rememberMe') с Checkbox */}
-
-            <Button type="submit">Войти</Button>
+            {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+            <FormControlLabel
+              label={"Remember me"}
+              control={
+                <Controller
+                  name={"rememberMe"}
+                  control={control}
+                  render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
+                />
+              }
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
           </FormGroup>
         </FormControl>
       </form>
