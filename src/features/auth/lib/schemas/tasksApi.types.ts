060658052eb4e9ce9dc32d29f1c635799038456c
Chernov-V <ikomaz97@gmail.com>
2026-01-09 19:24:18 +0400

    sprint 05 01 14

--- src/features/auth/lib/schemas/tasksApi.types.ts
+++ src/features/auth/lib/schemas/tasksApi.types.ts
@@ -1,17 +1,24 @@
-import { z } from "zod"
 import { TaskPriority, TaskStatus } from "@/common/enums"
+import { z } from "zod"
 
 export const domainTaskSchema = z.object({
-  description: z.string(),
-  deadline: z.string(),
-  startDate: z.string(),
+  description: z.string().nullable(),
   title: z.string(),
+  status: z.nativeEnum(TaskStatus),
+  priority: z.nativeEnum(TaskPriority),
+  startDate: z.string().nullable(),
+  deadline: z.string().nullable(),
   id: z.string(),
   todoListId: z.string(),
   order: z.int(),
   addedDate: z.string(),
-  status: z.enum(TaskStatus),
-  priority: z.enum(TaskPriority),
 })
 
-export type DomainTask = z.infer<typeof domainTaskSchema>
+export type UpdateTaskModel = {
+  description: string | null
+  title: string
+  status: TaskStatus
+  priority: TaskPriority
+  startDate: string | null
+  deadline: string | null
+}
