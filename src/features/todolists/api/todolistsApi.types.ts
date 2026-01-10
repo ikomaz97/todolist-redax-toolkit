c8d8ee743a133b13b5d8d715e6ca6c04b79038df
Chernov-V <ikomaz97@gmail.com>
2026-01-10 12:54:12 +0400

    sprint 05 01 17

--- src/features/todolists/api/todolistsApi.types.ts
+++ src/features/todolists/api/todolistsApi.types.ts
@@ -1,6 +1,10 @@
-export type Todolist = {
-  id: string
-  title: string
-  addedDate: string
-  order: number
-}
+import { z } from "zod/v4"
+
+export const todolistSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  addedDate: z.iso.datetime({ local: true }),
+  order: z.int(),
+})
+
+export type Todolist = z.infer<typeof todolistSchema>
