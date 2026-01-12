e638b314764f7859bb98419e0978344bb5ed5df7
Chernov-V <ikomaz97@gmail.com>
2026-01-12 10:54:39 +0400

    sprint 05 01 27

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,9 +1,15 @@
+import { AUTH_TOKEN } from "@/common/constants"
 import axios from "axios"
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    // ❌ Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
     "API-KEY": import.meta.env.VITE_API_KEY,
   },
+})
+
+instance.interceptors.request.use(function (config) {
+  config.headers["Authorization"] = `Bearer ${localStorage.getItem(AUTH_TOKEN)}`
+  return config
 })
