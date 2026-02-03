70ad2dfff6698164f677847ea991e15490951215
Chernov-V <ikomaz97@gmail.com>
2026-02-03 18:32:57 +0400

    28-01-022

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,39 +1,43 @@
 import { EditableSpan } from "@/common/components"
-import { useAppDispatch } from "@/common/hooks"
-import {
-  changeTodolistTitleTC,
-  deleteTodolistTC,
-  type DomainTodolist,
-} from "@/features/todolists/model/todolists-slice"
+import { DomainTodolist } from "@/features/todolists/api/todolistsApi"
 import DeleteIcon from "@mui/icons-material/Delete"
 import IconButton from "@mui/material/IconButton"
 import styles from "./TodolistTitle.module.css"
+import {
+    useRemoveTodolistMutation,
+    useUpdateTodolistTitleMutation,
+} from "@/features/todolists/api/todolistsApi"
 
 type Props = {
-  todolist: DomainTodolist
+    todolist: DomainTodolist
 }
 
 export const TodolistTitle = ({ todolist }: Props) => {
-  const { id, title, entityStatus } = todolist
+    const { id, title, entityStatus } = todolist
 
-  const dispatch = useAppDispatch()
+    const [removeTodolist] = useRemoveTodolistMutation()
+    const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
-  const deleteTodolist = () => {
-    dispatch(deleteTodolistTC(id))
-  }
+    const deleteTodolistHandler = () => {
+        removeTodolist(id)
+    }
 
-  const changeTodolistTitle = (title: string) => {
-    dispatch(changeTodolistTitleTC({ id, title }))
-  }
+    const changeTodolistTitleHandler = (title: string) => {
+        updateTodolistTitle({ id, title })
+    }
 
-  return (
-    <div className={styles.container}>
-      <h3>
-        <EditableSpan value={title} onChange={changeTodolistTitle} />
-      </h3>
-      <IconButton onClick={deleteTodolist} disabled={entityStatus === "loading"}>
-        <DeleteIcon />
-      </IconButton>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <h3>
+                <EditableSpan value={title} onChange={changeTodolistTitleHandler} />
+            </h3>
+
+            <IconButton
+                onClick={deleteTodolistHandler}
+                disabled={entityStatus === "loading"}
+            >
+                <DeleteIcon />
+            </IconButton>
+        </div>
+    )
 }
