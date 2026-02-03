0c5e1b2df8b311724c253c4f225e91cd4bf47823
Chernov-V <ikomaz97@gmail.com>
2026-02-03 18:31:40 +0400

    28-01-011

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,25 +1,25 @@
 import { CreateItemForm } from "@/common/components/CreateItemForm/CreateItemForm"
-import { useAppDispatch } from "@/common/hooks"
-import { createTodolistTC } from "@/features/todolists/model/todolists-slice"
 import { Todolists } from "@/features/todolists/ui/Todolists/Todolists"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
+import { useAddTodolistMutation } from "@/features/todolists/api/todolistsApi"
 
 export const Main = () => {
-  const dispatch = useAppDispatch()
+    const [addTodolist] = useAddTodolistMutation()
 
-  const createTodolist = (title: string) => {
-    dispatch(createTodolistTC(title))
-  }
+    const addTodolistHandler = (title: string) => {
+        addTodolist(title)
+    }
 
-  return (
-    <Container maxWidth={"lg"}>
-      <Grid container sx={{ mb: "30px" }}>
-        <CreateItemForm onCreateItem={createTodolist} />
-      </Grid>
-      <Grid container spacing={4}>
-        <Todolists />
-      </Grid>
-    </Container>
-  )
+    return (
+        <Container maxWidth="lg">
+            <Grid container sx={{ mb: "30px" }}>
+                <CreateItemForm onCreateItem={addTodolistHandler} />
+            </Grid>
+
+            <Grid container spacing={4}>
+                <Todolists />
+            </Grid>
+        </Container>
+    )
 }
