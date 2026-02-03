3d7cd4bfcecef7e890cc39a9cf435c42b037541e
Chernov-V <ikomaz97@gmail.com>
2026-02-03 18:31:45 +0400

    28-01-012

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,15 +3,21 @@ import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice
 import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
 import { configureStore } from "@reduxjs/toolkit"
 import { appReducer, appSlice } from "./app-slice.ts"
+import {todolistsApi} from "@/features/todolists/api/todolistsApi.ts";
+import {setupListeners} from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
-  reducer: {
-    [tasksSlice.name]: tasksReducer,
-    [todolistsSlice.name]: todolistsReducer,
-    [appSlice.name]: appReducer,
-    [authSlice.name]: authReducer,
-  },
+    reducer: {
+        [tasksSlice.name]: tasksReducer,
+        [todolistsSlice.name]: todolistsReducer,
+        [appSlice.name]: appReducer,
+        [authSlice.name]: authReducer,
+        [todolistsApi.reducerPath]: todolistsApi.reducer,
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(todolistsApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
