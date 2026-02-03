30d6b6241a1f2f9b27f91d8a53ab5e561b66774a
Chernov-V <ikomaz97@gmail.com>
2026-02-03 18:32:02 +0400

    28-01-015

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,28 +1,24 @@
-import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
-import { useEffect } from "react"
 import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { useGetTodolistsQuery } from "@/features/todolists/api/todolistsApi"
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
+    // Запрос выполняется автоматически при маунте компонента
+    const { data: todolists, refetch } = useGetTodolistsQuery()
 
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTodolistsTC())
-  }, [])
-
-  return (
-    <>
-      {todolists.map((todolist) => (
-        <Grid key={todolist.id}>
-          <Paper sx={{ p: "0 20px 20px 20px" }}>
-            <TodolistItem todolist={todolist} />
-          </Paper>
-        </Grid>
-      ))}
-    </>
-  )
+    return (
+        <>
+            <div>
+                <button onClick={refetch}>Получить свежие данные</button>
+            </div>
+            {todolists?.map((todolist) => (
+                <Grid key={todolist.id}>
+                    <Paper sx={{ p: "0 20px 20px 20px" }}>
+                        <TodolistItem todolist={todolist} />
+                    </Paper>
+                </Grid>
+            ))}
+        </>
+    )
 }
