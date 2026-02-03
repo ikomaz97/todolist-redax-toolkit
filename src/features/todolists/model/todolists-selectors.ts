06f997d49720bbaa2143103e106576756e45f27a
Chernov-V <ikomaz97@gmail.com>
2026-02-03 18:32:16 +0400

    28-01-017

--- src/features/todolists/model/todolists-selectors.ts
+++
@@ -1,4 +0,0 @@
-import type { RootState } from "@/app/store"
-import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
-
-export const selectTodolists = (state: RootState): Todolist[] => state.todolists
