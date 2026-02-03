0ab9ef0cfcfe92633b5d0aa1b0f73c4c8612ca8d
Chernov-V <ikomaz97@gmail.com>
2026-02-03 18:32:51 +0400

    28-01-021

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,19 +1,84 @@
-import { instance } from "@/common/instance"
-import type { BaseResponse } from "@/common/types"
-import type { Todolist } from "./todolistsApi.types"
+// Во избежание ошибок импорт должен быть из `@reduxjs/toolkit/query/react`
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { AUTH_TOKEN } from '@/common/constants'
+import { BaseResponse } from '@/common/types'
 
-export const todolistsApi = {
-  getTodolists() {
-    return instance.get<Todolist[]>("/todo-lists")
-  },
-  changeTodolistTitle(payload: { id: string; title: string }) {
-    const { id, title } = payload
-    return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
-  },
-  createTodolist(title: string) {
-    return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
-  },
-  deleteTodolist(id: string) {
-    return instance.delete<BaseResponse>(`/todo-lists/${id}`)
-  },
+/* ===================== TYPES ===================== */
+
+// То, что приходит с сервера
+export type Todolist = {
+    id: string
+    title: string
+    addedDate: string
+    order: number
 }
+
+export type FilterValues = 'all' | 'active' | 'completed'
+export type RequestStatus = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+// То, что используется на клиенте
+export type DomainTodolist = Todolist & {
+    filter: FilterValues
+    entityStatus: RequestStatus
+}
+
+/* ===================== API ===================== */
+
+export const todolistsApi = createApi({
+    reducerPath: 'todolistsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_BASE_URL,
+        headers: {
+            'API-KEY': import.meta.env.VITE_API_KEY,
+        },
+        prepareHeaders: headers => {
+            const token = localStorage.getItem(AUTH_TOKEN)
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+            return headers
+        },
+    }),
+    endpoints: build => ({
+        // ---------- GET ----------
+        getTodolists: build.query<DomainTodolist[], void>({
+            query: () => 'todo-lists',
+            transformResponse: (todolists: Todolist[]): DomainTodolist[] =>
+                todolists.map(todolist => ({
+                    ...todolist,
+                    filter: 'all',
+                    entityStatus: 'idle',
+                })),
+        }),
+
+        removeTodolist: build.mutation<BaseResponse, string>({
+            query: (id) => ({
+                url: `todo-lists/${id}`,
+                method: "DELETE",
+            }),
+        }),
+
+        // ---------- POST ----------
+        addTodolist: build.mutation<BaseResponse<{ item: Todolist }>, string>({
+            query: title => ({
+                url: 'todo-lists',
+                method: 'POST',
+                body: { title },
+            }),
+        }),
+        updateTodolistTitle: build.mutation<BaseResponse, { id: string; title: string }>({
+            query: ({ id, title }) => ({
+                url: `todo-lists/${id}`,
+                method: "PUT",
+                body: { title },
+            }),
+        }),
+    }),
+})
+
+export const {
+    useGetTodolistsQuery,
+    useAddTodolistMutation,
+    useRemoveTodolistMutation,
+    useUpdateTodolistTitleMutation,
+} = todolistsApi
