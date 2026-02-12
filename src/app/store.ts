5f166a838dc589b43ab08f9dd9c3029e279ed56c
Chernov-V <ikomaz97@gmail.com>
2026-02-12 13:48:36 +0400

    28-01-038

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,26 +1,29 @@
-import {authReducer, authSlice} from "@/features/auth/model/auth-slice"
-import {tasksReducer, tasksSlice} from "@/features/todolists/model/tasks-slice"
-import {todolistsReducer, todolistsSlice} from "@/features/todolists/model/todolists-slice"
-import {configureStore} from "@reduxjs/toolkit"
-import {appReducer, appSlice} from "./app-slice.ts"
-import {setupListeners} from "@reduxjs/toolkit/query";
-import {baseApi} from "@/app/baseApi.ts";
+import { configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+
+import { appReducer } from "@/app/app-slice"
+import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice"
+import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
+import { baseApi } from "@/app/baseApi.ts"
 
 export const store = configureStore({
     reducer: {
+        app: appReducer,                  // ✅ здесь app
         [tasksSlice.name]: tasksReducer,
         [todolistsSlice.name]: todolistsReducer,
-        [appSlice.name]: appReducer,
-        [authSlice.name]: authReducer,
         [baseApi.reducerPath]: baseApi.reducer,
     },
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware().concat(baseApi.middleware),
 })
+
+// подключаем RTK Query слушатели
 setupListeners(store.dispatch)
 
+// Типы для useSelector и useDispatch
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
-// для возможности обращения к store в консоли браузера
+// для консоли браузера
 // @ts-ignore
 window.store = store
