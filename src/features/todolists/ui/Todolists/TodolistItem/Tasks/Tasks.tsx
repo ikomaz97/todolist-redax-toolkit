08dddc8615fc7ef3330d66fd481c072e4f0753a3
Chernov-V <ikomaz97@gmail.com>
2026-02-20 15:15:57 +0400

    28-01-046

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,42 +1,48 @@
 import { TaskStatus } from "@/common/enums"
-import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { fetchTasksTC, selectTasks } from "@/features/todolists/model/tasks-slice"
 import type { DomainTodolist } from "@/features/todolists/model/todolists-slice"
-import { useEffect } from "react"
 import { TaskItem } from "./TaskItem/TaskItem"
 import List from "@mui/material/List"
 
+import { useGetTasksQuery } from "@/features/todolists/api/tasksApi"
+
 type Props = {
-  todolist: DomainTodolist
+    todolist: DomainTodolist
 }
 
 export const Tasks = ({ todolist }: Props) => {
-  const { id, filter } = todolist
+    const { id, filter } = todolist
 
-  const tasks = useAppSelector(selectTasks)
+    // ✅ RTK Query сам загружает таски
+    const { data, isLoading } = useGetTasksQuery(id)
 
-  const dispatch = useAppDispatch()
+    // если ещё грузится
+    if (isLoading) {
+        return <p>Загрузка...</p>
+    }
 
-  const todolistTasks = tasks[id]
-  let filteredTasks = todolistTasks
-  if (filter === "active") {
-    filteredTasks = todolistTasks.filter((task) => task.status === TaskStatus.New)
-  }
-  if (filter === "completed") {
-    filteredTasks = todolistTasks.filter((task) => task.status === TaskStatus.Completed)
-  }
+    // таски из ответа
+    let filteredTasks = data?.items ?? []
 
-  useEffect(() => {
-    dispatch(fetchTasksTC(id))
-  }, [])
+    // фильтрация
+    if (filter === "active") {
+        filteredTasks = filteredTasks.filter((task) => task.status === TaskStatus.New)
+    }
 
-  return (
-    <>
-      {filteredTasks?.length === 0 ? (
-        <p>Тасок нет</p>
-      ) : (
-        <List>{filteredTasks?.map((task) => <TaskItem key={task.id} task={task} todolist={todolist} />)}</List>
-      )}
-    </>
-  )
+    if (filter === "completed") {
+        filteredTasks = filteredTasks.filter((task) => task.status === TaskStatus.Completed)
+    }
+
+    return (
+        <>
+            {filteredTasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <List>
+                    {filteredTasks.map((task) => (
+                        <TaskItem key={task.id} task={task} todolist={todolist} />
+                    ))}
+                </List>
+            )}
+        </>
+    )
 }
