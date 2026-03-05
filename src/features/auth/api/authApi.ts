aee6a20eedb48cf5663e9c288a6fc0b9fc740624
Chernov-V <ikomaz97@gmail.com>
2026-03-05 10:10:46 +0400

    09

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,38 +1,19 @@
-import { BaseResponse } from "@/common/types"
 import { baseApi } from "@/app/baseApi.ts"
-
-// ✅ Тип для данных логина
-export type LoginArgs = {
-    email: string
-    password: string
-    rememberMe?: boolean
-}
+import type { BaseResponse } from "@/common/types"
+import type { LoginInputs } from "@/features/auth/lib/schemas"
 
 export const authApi = baseApi.injectEndpoints({
-    endpoints: build => ({
-        // Проверка авторизации "Кто я?"
-        me: build.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
-            query: () => "auth/me",
-        }),
-
-        // Логин
-        login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginArgs>({
-            query: body => ({
-                url: "auth/login",
-                method: "POST",
-                body,
-            }),
-        }),
-
-        // Логаут
-        logout: build.mutation<BaseResponse, void>({
-            query: () => ({
-                url: "auth/logout",
-                method: "DELETE",
-            }),
-        }),
+  endpoints: (builder) => ({
+    login: builder.mutation<BaseResponse<{ userId: number; token: string }>, LoginInputs>({
+      query: (body) => ({ method: "post", url: "auth/login", body }),
     }),
+    logout: builder.mutation<BaseResponse, void>({
+      query: () => ({ method: "delete", url: "auth/login" }),
+    }),
+    me: builder.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
+      query: () => "auth/me",
+    }),
+  }),
 })
 
-// ✅ Хуки для компонентов
-export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi
+export const { useLoginMutation, useLogoutMutation, useMeQuery } = authApi
