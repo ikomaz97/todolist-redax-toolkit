07b70681d98d880dbb4b6a84238257921b4e08df
Chernov-V <ikomaz97@gmail.com>
2026-03-05 10:10:38 +0400

    08

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,8 +1,10 @@
 import { selectThemeMode, setIsLoggedInAC } from "@/app/app-slice"
+import { AUTH_TOKEN } from "@/common/constants"
+import { ResultCode } from "@/common/enums"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
+import { useLoginMutation } from "@/features/auth/api/authApi"
 import { type LoginInputs, loginSchema } from "@/features/auth/lib/schemas"
-
 import { zodResolver } from "@hookform/resolvers/zod"
 import Button from "@mui/material/Button"
 import Checkbox from "@mui/material/Checkbox"
@@ -13,88 +15,89 @@ import FormLabel from "@mui/material/FormLabel"
 import Grid from "@mui/material/Grid"
 import TextField from "@mui/material/TextField"
 import { Controller, type SubmitHandler, useForm } from "react-hook-form"
-import { useLoginMutation } from "@/features/auth/api/authApi"
 import styles from "./Login.module.css"
 
 export const Login = () => {
-    const themeMode = useAppSelector(selectThemeMode)
-    const dispatch = useAppDispatch()
-    const theme = getTheme(themeMode)
+  const themeMode = useAppSelector(selectThemeMode)
 
-    const [loginMutation] = useLoginMutation()
+  const [login] = useLoginMutation()
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        control,
-        formState: { errors },
-    } = useForm<LoginInputs>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: { email: "", password: "", rememberMe: false },
+  const dispatch = useAppDispatch()
+
+  const theme = getTheme(themeMode)
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<LoginInputs>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: { email: "", password: "", rememberMe: false },
+  })
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+    login(data).then((res) => {
+      if (res.data?.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedInAC({ isLoggedIn: true }))
+        localStorage.setItem(AUTH_TOKEN, res.data.data.token)
+        reset()
+      }
     })
+  }
 
-    const onSubmit: SubmitHandler<LoginInputs> = (data) => {
-        loginMutation(data).then(res => {
-            if (res.data?.resultCode === 0) { // ResultCode.Success
-                dispatch(setIsLoggedInAC({ isLoggedIn: true }))
-                localStorage.setItem("AUTH_TOKEN", res.data.data.token)
-                reset()
-            }
-        })
-    }
-
-    return (
-        <Grid container justifyContent={"center"}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl>
-                    <FormLabel>
-                        <p>
-                            To login get registered
-                            <a
-                                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
-                                href="https://social-network.samuraijs.com"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                here
-                            </a>
-                        </p>
-                        <p>or use common test account credentials:</p>
-                        <p>
-                            <b>Email:</b> free@samuraijs.com
-                        </p>
-                        <p>
-                            <b>Password:</b> free
-                        </p>
-                    </FormLabel>
-                    <FormGroup>
-                        <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
-                        {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
-                        <TextField
-                            type="password"
-                            label="Password"
-                            margin="normal"
-                            error={!!errors.password}
-                            {...register("password")}
-                        />
-                        {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
-                        <FormControlLabel
-                            label={"Remember me"}
-                            control={
-                                <Controller
-                                    name={"rememberMe"}
-                                    control={control}
-                                    render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
-                                />
-                            }
-                        />
-                        <Button type="submit" variant="contained" color="primary">
-                            Login
-                        </Button>
-                    </FormGroup>
-                </FormControl>
-            </form>
-        </Grid>
-    )
+  return (
+    <Grid container justifyContent={"center"}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+                href="https://social-network.samuraijs.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
+          <FormGroup>
+            <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+            <TextField
+              type="password"
+              label="Password"
+              margin="normal"
+              error={!!errors.email}
+              {...register("password")}
+            />
+            {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+            <FormControlLabel
+              label={"Remember me"}
+              control={
+                <Controller
+                  name={"rememberMe"}
+                  control={control}
+                  render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
+                />
+              }
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
+    </Grid>
+  )
 }
