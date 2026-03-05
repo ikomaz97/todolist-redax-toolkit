af9ece7e6c9d170294d5f587480108be2b779eb9
Chernov-V <ikomaz97@gmail.com>
2026-03-05 10:10:18 +0400

    06

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,27 +1,25 @@
+import { baseApi } from "@/app/baseApi"
+import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice"
+import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
 import { configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-
-import { appReducer } from "@/app/app-slice"
-import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
-import { baseApi } from "@/app/baseApi.ts"
+import { appReducer, appSlice } from "./app-slice.ts"
 
 export const store = configureStore({
-    reducer: {
-        app: appReducer,                  // ✅ app slice
-        [todolistsSlice.name]: todolistsReducer,
-        [baseApi.reducerPath]: baseApi.reducer, // ✅ RTK Query
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(baseApi.middleware),
+  reducer: {
+    [tasksSlice.name]: tasksReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [appSlice.name]: appReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
-// подключаем RTK Query слушатели
 setupListeners(store.dispatch)
 
-// Типы для useSelector и useDispatch
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
-// для консоли браузера
+// для возможности обращения к store в консоли браузера
 // @ts-ignore
 window.store = store
