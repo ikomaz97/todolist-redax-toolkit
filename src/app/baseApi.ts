21b3ee4f2bc05141d2bca9beaa505d2300416032
Chernov-V <ikomaz97@gmail.com>
2026-03-08 12:10:07 +0400

    24

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,17 +1,28 @@
 import { AUTH_TOKEN } from "@/common/constants"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import {setAppErrorAC} from "@/app/app-slice.ts";
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task"],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
+    baseQuery: async (args, api, extraOptions) => {
+        const result = await fetchBaseQuery({
+            baseUrl: import.meta.env.VITE_BASE_URL,
+            headers: {
+                'API-KEY': import.meta.env.VITE_API_KEY,
+            },
+            prepareHeaders: headers => {
+                headers.set('Authorization', `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+            },
+        })(args, api, extraOptions)
+
+        if (result.error) {
+            if (result.error.status === 'FETCH_ERROR') {
+                api.dispatch(setAppErrorAC({ error: result.error.error }))
+            }
+        }
+
+        return result
     },
-    prepareHeaders: (headers) => {
-      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
-    },
-  }),
   endpoints: () => ({}),
 })
