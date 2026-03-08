ded3a18ff27b24ff8bea67cf57ee740515c504d0
Chernov-V <ikomaz97@gmail.com>
2026-03-08 12:11:37 +0400

    29

--- src/common/utils/isErrorWithMessage.ts
+++ src/common/utils/isErrorWithMessage.ts
@@ -1,6 +1,8 @@
 
 export function isErrorWithMessage(error: unknown): error is { message: string } {
     return (
+
+
         typeof error === 'object' && // Проверяем, что error – это объект
         error != null && // Убеждаемся, что это не null
         'message' in error && // Проверяем, что у объекта есть свойство 'message'
