9d788d259cde0dc2b2fd11bafa49040198c2d1ee
Chernov-V <ikomaz97@gmail.com>
2026-03-09 17:29:24 +0400

    30

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -29,7 +29,7 @@ export const App = () => {
       dispatch(setIsLoggedInAC({ isLoggedIn: true }))
     }
     setIsInitialized(true)
-  }, [isLoading])
+  }, [isLoading, data, dispatch])
 
   if (!isInitialized) {
     return (
