f42cd83e5c2c427a9371d35dc2e8579fedb43436
Chernov-V <ikomaz97@gmail.com>
2026-03-10 15:33:58 +0400

    35

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,28 +1,24 @@
 import { AUTH_TOKEN } from "@/common/constants"
+import { handleError } from "@/common/utils"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import {setAppErrorAC} from "@/app/app-slice.ts";
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task"],
-    baseQuery: async (args, api, extraOptions) => {
-        const result = await fetchBaseQuery({
-            baseUrl: import.meta.env.VITE_BASE_URL,
-            headers: {
-                'API-KEY': import.meta.env.VITE_API_KEY,
-            },
-            prepareHeaders: headers => {
-                headers.set('Authorization', `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
-            },
-        })(args, api, extraOptions)
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      headers: {
+        'API-KEY': import.meta.env.VITE_API_KEY,
+      },
+      prepareHeaders: (headers) => {
+        headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+      },
+    })(args, api, extraOptions)
 
-        if (result.error) {
-            if (result.error.status === 'FETCH_ERROR') {
-                api.dispatch(setAppErrorAC({ error: result.error.error }))
-            }
-        }
+    handleError(api, result)
 
-        return result
-    },
+    return result
+  },
   endpoints: () => ({}),
 })
