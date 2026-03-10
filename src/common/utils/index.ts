fe46a5f21ca3bf2f50fca6ccb5be2b53a8d11c7b
Chernov-V <ikomaz97@gmail.com>
2026-03-10 15:34:19 +0400

    38

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,3 @@
 export { createAppSlice } from "./createAppSlice"
-export { handleServerAppError } from "./handleServerAppError"
-export { handleServerNetworkError } from "./handleServerNetworkError"
+export { handleError } from "./handleError"
+export { isErrorWithMessage } from "./isErrorWithMessage"
