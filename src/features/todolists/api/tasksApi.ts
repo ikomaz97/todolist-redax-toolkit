cd61a617e2fdd63b22ae8769e0d09b623d4cd53d
Chernov-V <ikomaz97@gmail.com>
2026-03-11 11:24:54 +0400

    44

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -4,25 +4,28 @@ import type { BaseResponse } from "@/common/types"
 import type { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
 
 export const tasksApi = baseApi.injectEndpoints({
-  endpoints: (build) => ({
-    getTasks: build.query<GetTasksResponse, string>({
-      query: (todolistId) => `todo-lists/${todolistId}/tasks`,
-      providesTags: ["Task"],
-    }),
+    endpoints: (build) => ({
+        getTasks: build.query<GetTasksResponse, string>({
+            query: (todolistId) => `todo-lists/${todolistId}/tasks`,
+            providesTags: (res, _err, todolistId) =>
+                res
+                    ? [...res.items.map(({ id }) => ({ type: "Task", id }) as const), { type: "Task", id: todolistId }]
+                    : ["Task"],
+        }),
     addTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
       query: ({ todolistId, title }) => ({
         url: `todo-lists/${todolistId}/tasks`,
         method: "POST",
         body: { title },
       }),
-      invalidatesTags: ["Task"],
+        invalidatesTags: res => [{ type: 'Task', id: res ? res.data.item.id : 'LIST' }],
     }),
     removeTask: build.mutation<BaseResponse, { todolistId: string; taskId: string }>({
       query: ({ todolistId, taskId }) => ({
         url: `todo-lists/${todolistId}/tasks/${taskId}`,
         method: "DELETE",
       }),
-      invalidatesTags: ["Task"],
+        invalidatesTags: (_res, _err, { taskId }) => [{ type: "Task", id: taskId }],
     }),
     updateTask: build.mutation<
       BaseResponse<{ item: DomainTask }>,
