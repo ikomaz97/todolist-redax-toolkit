f90c3ef83847e40dea69ed0e475c1906e9558a0e
Chernov-V <ikomaz97@gmail.com>
2026-03-17 10:24:26 +0400

    897115

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -1,26 +1,18 @@
 import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import { TaskStatus } from "@/common/enums"
-
 import { useRemoveTaskMutation, useUpdateTaskMutation } from "@/features/todolists/api/tasksApi"
-
 import type { DomainTask } from "@/features/todolists/api/tasksApi.types"
 import type { DomainTodolist } from "@/features/todolists/lib/types"
-
 import { createTaskModel } from "@/features/todolists/lib/utils"
-
 import DeleteIcon from "@mui/icons-material/Delete"
-
 import Checkbox from "@mui/material/Checkbox"
 import IconButton from "@mui/material/IconButton"
 import ListItem from "@mui/material/ListItem"
 import Box from "@mui/material/Box"
-
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
-
 import { motion } from "framer-motion"
 import { useState } from "react"
-
 import Snackbar from "@mui/material/Snackbar"
 import Alert from "@mui/material/Alert"
 
@@ -33,13 +25,17 @@ type Props = {
 export const TaskItem = ({ task, todolist, isLast }: Props) => {
   const [removeTask] = useRemoveTaskMutation()
   const [updateTask] = useUpdateTaskMutation()
-
   const [snackbarOpen, setSnackbarOpen] = useState(false)
 
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
     id: task.id,
     data: {
-      task,
+      type: "task",
+      task: {
+        id: task.id,
+        title: task.title,
+        status: task.status,
+      },
     },
   })
 
@@ -62,9 +58,7 @@ export const TaskItem = ({ task, todolist, isLast }: Props) => {
 
   const changeTaskStatus = (e: React.ChangeEvent<HTMLInputElement>) => {
     const status = e.currentTarget.checked ? TaskStatus.Completed : TaskStatus.New
-
     const model = createTaskModel(task, { status })
-
     updateTask({
       taskId: task.id,
       todolistId: todolist.id,
@@ -75,9 +69,7 @@ export const TaskItem = ({ task, todolist, isLast }: Props) => {
   const changeTaskTitle = (title: string) => {
     const trimmed = title.trim()
     if (!trimmed) return
-
     const model = createTaskModel(task, { title: trimmed })
-
     updateTask({
       taskId: task.id,
       todolistId: todolist.id,
@@ -118,10 +110,8 @@ export const TaskItem = ({ task, todolist, isLast }: Props) => {
             }}
           >
             <Checkbox checked={isCompleted} onChange={changeTaskStatus} size="small" sx={{ p: 0.5, mr: 1 }} />
-
             <EditableSpan value={task.title} onChange={changeTaskTitle} maxDisplayLength={15} />
           </Box>
-
           <IconButton onClick={deleteTask} size="small">
             <DeleteIcon fontSize="small" />
           </IconButton>
