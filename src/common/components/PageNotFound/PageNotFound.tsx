9c62fbc6e1311cc325b09929dfc166358a5316e6
Chernov-V <ikomaz97@gmail.com>
2026-03-19 22:32:37 +0400

    21146891014444eessdd

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -6,8 +6,8 @@ import styles from "./PageNotFound.module.css"
 
 export const PageNotFound = () => (
   <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-    <h1 className={styles.title}>404</h1>
-    <h2 className={styles.subtitle}>page not found</h2>
+    <h1 className={styles.title}>Вернуться на главную</h1>
+    <h2 className={styles.subtitle}>Вернуться на главную</h2>
     <Button variant="contained" component={Link} to={Path.Main} sx={{ width: "330px", mt: "20px" }}>
       Вернуться на главную
     </Button>
