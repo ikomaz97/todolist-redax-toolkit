c752a7b5381406866ef02017ae004b9bff62814e
Chernov-V <ikomaz97@gmail.com>
2026-03-19 21:44:48 +0400

    21146891014444ee

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todolist-redax-toolkit/", // 👈 ЭТО НУЖНО ДОБАВИТЬ
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
