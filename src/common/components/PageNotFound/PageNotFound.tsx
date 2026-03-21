14f86e16aec77f79e180e2b9d43890ea2697a7ca
Chernov-V <ikomaz97@gmail.com>
2026-03-21 22:23:29 +0400

    21-03-2026

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,14 +1,58 @@
 import { Path } from "@/common/routing"
 import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
 import { Link } from "react-router"
-import styles from "./PageNotFound.module.css"
 
 export const PageNotFound = () => (
-  <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+  <Container
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      height: "100vh",
+      pt: "25vh", // ⬅️ двигает вверх/вниз
+    }}
+  >
+    <Typography
+      sx={{
+        textAlign: "center",
+        lineHeight: 1.2,
+        fontSize: {
+          xs: "1.2rem", // мобилка
+          sm: "1.5rem", // планшет
+          md: "2rem", // десктоп
+        },
+      }}
+    >
+      Пожалуйста, <br />
+      авторизуйтесь
+    </Typography>
 
-    <h6 className={styles.subtitle}>Пожалйста, авторизуйтесь</h6>
-    <Button variant="contained" component={Link} to={Path.Main} sx={{ width: "330px", mt: "20px" }}>
+    <Button
+      variant="contained"
+      component={Link}
+      to={Path.Main}
+      sx={{
+        mt: 3,
+        width: {
+          xs: "100%", // на мобилке во всю ширину
+          sm: "350px",
+          md: "500px",
+        },
+        fontSize: {
+          xs: "0.9rem",
+          sm: "1.1rem",
+          md: "1.3rem",
+        },
+        padding: {
+          xs: "10px",
+          sm: "12px",
+          md: "14px",
+        },
+      }}
+    >
       Вернуться на страницу авторизации
     </Button>
   </Container>
