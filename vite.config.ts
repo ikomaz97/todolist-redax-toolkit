c94fc386578bd6e28eed5ecf7e625670fb859bbf
Chernov-V <ikomaz97@gmail.com>
2026-06-18 13:08:10 +0400

    vercel5

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
-  resolve: {
-    alias: {
-      "@/": `${path.resolve(__dirname, "src")}/`,
+export default defineConfig(({ mode }) => {
+  // Для GitHub Pages используем basePath, для Vercel — root
+  const base = mode === 'gh-pages' ? '/todolist-redax-toolkit/' : '/'
+  
+  return {
+    plugins: [react()],
+    base,
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+      },
     },
-  },
+  }
 })
